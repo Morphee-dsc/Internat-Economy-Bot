221efa2b62d741e4945fdd228b631a201ef42c7a
Morphée-dsc <128927239+Morphee-dsc@users.noreply.github.com>
2023-06-13 13:46:15 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    token: 'MTExNzk0NTE2OTE2NjIwNTA0OA.GUMThy.dn65t-olzoFWhNHkP_3s_KdwAkxvODbU4oCADU', //token du bot
+    prefix: ".", //prefix du bot
+    color: "#2f3136", //couleur du bot
+    owner: "1109559949513732309", //id du owner du bot
+    webhook: { //pour que ça envoie automatiquement les backkups du bot
+        id: '1117944984449065051', 
+        token: 'uSrjchiBvHYtXGB_DLCUkNk7ziPg7T9Z_E0HqiKgnL02TkE1vA6MqczclNpDrlcXQGYD'
+    }
+}
